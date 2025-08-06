ec65d373d71cb3b92e2acb86e1a8887035e9ba5e
Curtis Puetz <puetzcurtis@gmail.com>
2025-08-06 13:29:52 -0700

    copy all relevant code from pypp project over here

---
+++ pypp_python/lists.py
@@ -0,0 +1,7 @@
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def list_reserve(arr: list[T], size: int) -> None:
+    pass
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Py++ Python
+This library contains necessary python code to work with Py++.
