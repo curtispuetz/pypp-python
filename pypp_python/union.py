b7afef9b2717ac1b7acdeba0f7a352c8627a8f61
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-09 11:14:51 -0700

    support printing and comparison operators for Uni

--- pypp_python/union.py
+++ pypp_python/union.py
@@ -5,6 +5,11 @@ class Uni[*Ts]:
     def __init__(self, value: Any):
         self._value = value
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Uni):
+            raise ValueError("Can only compare Uni with Uni")
+        return self._value == other._value
+
 
 def isinst[*Ts, T](obj: Uni[*Ts], _type: type[T]) -> bool:
     return isinstance(obj._value, _type)
